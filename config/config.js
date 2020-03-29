2afd00698f9faffed753df84386aebf2e8a03872
Avishay Matayev <me@avishay.dev>
2020-03-29 07:08:09 +0300

    Changed config

--- config/config.js
+++ config/config.js
@@ -1,24 +1,25 @@
 module.exports = {
   development: {
-    username: "user1",
-    password: "password1",
-    database: "database_development",
+    username: "groupin",
+    password: "Passwordm0reSeCret",
+    database: "groupin",
     host: "localhost",
-    dialect: "mysql"
+    dialect: "mysql",
   },
   test: {
     username: "user1",
     password: "password1",
     database: "database_test",
     host: "localhost",
-    dialect: "mysql"
+    dialect: "mysql",
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOSTNAME,
-    dialect: 'mysql',
-    use_env_variable: 'DATABASE_URL'
-  }
+    dialect: "mysql",
+    use_env_variable: "DATABASE_URL",
+  },
 };
+
