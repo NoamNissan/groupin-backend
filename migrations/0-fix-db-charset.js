8192ad16b26f59f0df7612416553fb9efcddcd0c
Avishay Matayev <me@avishay.dev>
2020-03-30 19:53:15 +0300

    Fix Sequelize DB charset encoding
    The hack was taken from here https://github.com/sequelize/cli/issues/590#issuecomment-369876816,
    this will be used until a better solution is found / the bug is fixed

---
+++ migrations/0-fix-db-charset.js
@@ -0,0 +1,13 @@
+// This is a hack to fix Sequelize's charset encoding setting
+//
+// Oddly Sequelize ignores the configuration in config.js, so we'll just add
+// a migration that will always be first and fix the database encoding
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query(
+      `ALTER DATABASE ${queryInterface.sequelize.config.database}
+        CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;`
+    );
+  },
+  down: (queryInterface, Sequelize) => {},
+};
