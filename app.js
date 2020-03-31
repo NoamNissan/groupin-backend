76343b55c01245599aba3c7f9b286a9ee391ef86
Avishay Matayev <me@avishay.dev>
2020-03-31 18:21:40 +0300

    Create .prettierrc and reformat files

--- app.js
+++ app.js
@@ -1,79 +1,79 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
-var mysql = require("mysql");
-const env = process.env.NODE_ENV || "development";
-var dbconfig = require("./config/config.js")[env];
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+var cors = require('cors')
+var mysql = require('mysql')
+const env = process.env.NODE_ENV || 'development'
+var dbconfig = require('./config/config.js')[env]
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
 
-var db = require("./models");
+var db = require('./models')
 
 // setup DB connection
 var connection = mysql.createConnection({
-  host: dbconfig.host,
-  port: dbconfig.port,
-  user: dbconfig.username,
-  password: dbconfig.password,
-});
+    host: dbconfig.host,
+    port: dbconfig.port,
+    user: dbconfig.username,
+    password: dbconfig.password,
+})
 
 connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+    if (err) {
+        console.error('error connecting: ' + err.stack)
+        return
+    }
 
-  console.log("connected as id " + connection.threadId);
-});
+    console.log('connected as id ' + connection.threadId)
+})
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-app.use(cors());
+app.use(cors())
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
-var graphqlHTTP = require("express-graphql");
-var { schema } = require("./schemas/api_schema");
+var graphqlHTTP = require('express-graphql')
+var { schema } = require('./schemas/api_schema')
 
 // The root provides a resolver function for each API endpoint
 var root = app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true,
-    context: { db: db },
-  })
-);
+    '/graphql',
+    graphqlHTTP({
+        schema: schema,
+        graphiql: true,
+        context: { db: db },
+    })
+)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
-});
+    next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
+})
 
-module.exports = app;
+module.exports = app
--- schemas/api_schema.js
+++ schemas/api_schema.js
@@ -1,38 +1,38 @@
-var { makeExecutableSchema } = require("graphql-tools");
-var FormatError = require("easygraphql-format-error");
+var { makeExecutableSchema } = require('graphql-tools')
+var FormatError = require('easygraphql-format-error')
 
 // Don't return more than MAX_SESSIONS_COUNT sessions in a single request!
-const MAX_SESSIONS_COUNT = 50;
+const MAX_SESSIONS_COUNT = 50
 
 const errors = new FormatError([
-  {
-    name: "COUNT_TOO_HIGH",
-    message: "The passed count value is too high",
-    statusCode: 400,
-  },
-]).errorName;
+    {
+        name: 'COUNT_TOO_HIGH',
+        message: 'The passed count value is too high',
+        statusCode: 400,
+    },
+]).errorName
 
 function strip_null(obj) {
-  Object.keys(obj).forEach((key) => obj[key] == null && delete obj[key]);
-  return obj;
+    Object.keys(obj).forEach((key) => obj[key] == null && delete obj[key])
+    return obj
 }
 
 // TODO: remove when login is implemented
 // TODO: all instances of user_token should be swapped with some real login auth token
-const USER_AUTHENTICATED = true;
+const USER_AUTHENTICATED = true
 
 function check_auth(token) {
-  if (USER_AUTHENTICATED) {
-    // TODO: switch to the real user id
-    return "1";
-  }
+    if (USER_AUTHENTICATED) {
+        // TODO: switch to the real user id
+        return '1'
+    }
 
-  throw new Error(errors.UNAUTHORIZED);
+    throw new Error(errors.UNAUTHORIZED)
 }
 
 const schema = makeExecutableSchema({
-  typeDefs: [
-    `
+    typeDefs: [
+        `
 scalar Date
 
 type User {
@@ -104,166 +104,176 @@ type Mutation {
     deleteSession(user_token: ID!, session_id: ID): Boolean!
 }
 `,
-  ],
-  resolvers: {
-    Query: {
-      // Hello is left for sanity, TODO remove when saner
-      hello: () => "Hello world!",
-      // A Stub, currently, fix when implementing login / register
-      User: () => ({
-        id: "5",
-        name: "Johnny Test",
-        email: "billgates@google.com",
-      }),
-      Session: (parnet, { id }, { db }, info) => db.Session.findByPk(id),
-      Categories: (parent, args, { db }, info) => db.Category.findAll(),
-      FrontSessions: (parent, { start, count }, { db }, info) => {
-        if (count > MAX_SESSIONS_COUNT) {
-          throw new Error(errors.COUNT_TOO_HIGH);
-        }
-        return db.Session.findAll({ offset: start, limit: count });
-      },
-      SessionsByUser: (parent, { username, start, count }, { db }, info) => {
-        if (count > MAX_SESSIONS_COUNT) {
-          throw new Error(errors.COUNT_TOO_HIGH);
-        }
-        return db.Session.findAll({
-          where: { username: { [Op.eq]: username } },
-        });
-      },
-      SessionsByCategory: (
-        parent,
-        { category, start, count },
-        { db },
-        info
-      ) => {
-        if (count > MAX_SESSIONS_COUNT) {
-          throw new Error(errors.COUNT_TOO_HIGH);
-        }
-        return db.Session.findAll({
-          where: { category: { [Op.eq]: category } },
-        });
-      },
-      ResessionsByUser: (parent, { username, start, count }, { db }, info) => {
-        if (count > MAX_SESSIONS_COUNT) {
-          throw new Error(errors.COUNT_TOO_HIGH);
-        }
-        return db.Resession.findAll({
-          where: { username: { [Op.eq]: username } },
-        });
-      },
-      ResessionsByCategory: (
-        parent,
-        { category, start, count },
-        { db },
-        info
-      ) => {
-        if (count > MAX_SESSIONS_COUNT) {
-          throw new Error(errors.COUNT_TOO_HIGH);
-        }
-        return db.Resession.findAll({
-          where: { category: { [Op.eq]: category } },
-        });
-      },
-      // TODO: Implement login
-      // login: () => {
-      //   return "5";
-      // },
-    },
-    Mutation: {
-      createSession: (
-        parent,
-        { user_token, title, category },
-        { db },
-        info
-      ) => {
-        const user_id = check_auth(user_token);
-        const tomorrow = new Date();
-        tomorrow.setDate(new Date().getDate() + 1);
-        return db.Session.create({
-          user_id: user_id,
-          title: title,
-          category: category,
-          start_date: tomorrow,
-          end_date: tomorrow,
-        });
-      },
-      editSession: async (
-        parent,
-        {
-          user_token,
-          session_id,
-          title,
-          description,
-          category,
-          tags,
-          start_date,
-          end_date,
-          capacity,
-          attendees,
-          platform,
-          platform_media_id,
-          img_source,
-        },
-        { db },
-        info
-      ) => {
-        // TODO: sanitize the date (end > start)
-        const user_id = check_auth(user_token);
-        const affected_rows = (
-          await db.Session.update(
-            strip_null({
-              title: title,
-              description: description,
-              category: category,
-              tags: tags,
-              start_date: start_date,
-              end_date: end_date,
-              capacity: capacity,
-              attendees: attendees,
-              platform: platform,
-              platform_media_id: platform_media_id,
-              img_source: img_source,
+    ],
+    resolvers: {
+        Query: {
+            // Hello is left for sanity, TODO remove when saner
+            hello: () => 'Hello world!',
+            // A Stub, currently, fix when implementing login / register
+            User: () => ({
+                id: '5',
+                name: 'Johnny Test',
+                email: 'billgates@google.com',
             }),
-            {
-              where: { id: session_id, user_id: user_id },
-            }
-          )
-        )[0];
+            Session: (parnet, { id }, { db }, info) => db.Session.findByPk(id),
+            Categories: (parent, args, { db }, info) => db.Category.findAll(),
+            FrontSessions: (parent, { start, count }, { db }, info) => {
+                if (count > MAX_SESSIONS_COUNT) {
+                    throw new Error(errors.COUNT_TOO_HIGH)
+                }
+                return db.Session.findAll({ offset: start, limit: count })
+            },
+            SessionsByUser: (
+                parent,
+                { username, start, count },
+                { db },
+                info
+            ) => {
+                if (count > MAX_SESSIONS_COUNT) {
+                    throw new Error(errors.COUNT_TOO_HIGH)
+                }
+                return db.Session.findAll({
+                    where: { username: { [Op.eq]: username } },
+                })
+            },
+            SessionsByCategory: (
+                parent,
+                { category, start, count },
+                { db },
+                info
+            ) => {
+                if (count > MAX_SESSIONS_COUNT) {
+                    throw new Error(errors.COUNT_TOO_HIGH)
+                }
+                return db.Session.findAll({
+                    where: { category: { [Op.eq]: category } },
+                })
+            },
+            ResessionsByUser: (
+                parent,
+                { username, start, count },
+                { db },
+                info
+            ) => {
+                if (count > MAX_SESSIONS_COUNT) {
+                    throw new Error(errors.COUNT_TOO_HIGH)
+                }
+                return db.Resession.findAll({
+                    where: { username: { [Op.eq]: username } },
+                })
+            },
+            ResessionsByCategory: (
+                parent,
+                { category, start, count },
+                { db },
+                info
+            ) => {
+                if (count > MAX_SESSIONS_COUNT) {
+                    throw new Error(errors.COUNT_TOO_HIGH)
+                }
+                return db.Resession.findAll({
+                    where: { category: { [Op.eq]: category } },
+                })
+            },
+            // TODO: Implement login
+            // login: () => {
+            //   return "5";
+            // },
+        },
+        Mutation: {
+            createSession: (
+                parent,
+                { user_token, title, category },
+                { db },
+                info
+            ) => {
+                const user_id = check_auth(user_token)
+                const tomorrow = new Date()
+                tomorrow.setDate(new Date().getDate() + 1)
+                return db.Session.create({
+                    user_id: user_id,
+                    title: title,
+                    category: category,
+                    start_date: tomorrow,
+                    end_date: tomorrow,
+                })
+            },
+            editSession: async (
+                parent,
+                {
+                    user_token,
+                    session_id,
+                    title,
+                    description,
+                    category,
+                    tags,
+                    start_date,
+                    end_date,
+                    capacity,
+                    attendees,
+                    platform,
+                    platform_media_id,
+                    img_source,
+                },
+                { db },
+                info
+            ) => {
+                // TODO: sanitize the date (end > start)
+                const user_id = check_auth(user_token)
+                const affected_rows = (
+                    await db.Session.update(
+                        strip_null({
+                            title: title,
+                            description: description,
+                            category: category,
+                            tags: tags,
+                            start_date: start_date,
+                            end_date: end_date,
+                            capacity: capacity,
+                            attendees: attendees,
+                            platform: platform,
+                            platform_media_id: platform_media_id,
+                            img_source: img_source,
+                        }),
+                        {
+                            where: { id: session_id, user_id: user_id },
+                        }
+                    )
+                )[0]
 
-        return affected_rows === 1;
-      },
+                return affected_rows === 1
+            },
 
-      // TODO: Implement resession
-      // No planned front end support for now
-      // editResession: (parent, args, { db }, info) => true,
-      // editResession: (parent, args, { db }, info) => true,
+            // TODO: Implement resession
+            // No planned front end support for now
+            // editResession: (parent, args, { db }, info) => true,
+            // editResession: (parent, args, { db }, info) => true,
 
-      // TODO: Implement deletion (decide on a deleted bit vs different table)
-      // This is not important enough for the demo
-      // deleteSession: (parent, args, { db }, info) => true,
-      // deleteResession: (parent, args, { db }, info) => true,
+            // TODO: Implement deletion (decide on a deleted bit vs different table)
+            // This is not important enough for the demo
+            // deleteSession: (parent, args, { db }, info) => true,
+            // deleteResession: (parent, args, { db }, info) => true,
 
-      // TODO: Implement register
-      // register: (parent, args, { db }, info) => {
-      //   return true;
-      // },
+            // TODO: Implement register
+            // register: (parent, args, { db }, info) => {
+            //   return true;
+            // },
+        },
+        Date: {
+            __parseValue(value) {
+                return new Date(value) // value from the client
+            },
+            __serialize(value) {
+                return value.getTime() // value sent to the client
+            },
+            __parseLiteral(ast) {
+                if (ast.kind === Kind.INT) {
+                    return parseInt(ast.value, 10) // ast value is always in string format
+                }
+                return null
+            },
+        },
     },
-    Date: {
-      __parseValue(value) {
-        return new Date(value); // value from the client
-      },
-      __serialize(value) {
-        return value.getTime(); // value sent to the client
-      },
-      __parseLiteral(ast) {
-        if (ast.kind === Kind.INT) {
-          return parseInt(ast.value, 10); // ast value is always in string format
-        }
-        return null;
-      },
-    },
-  },
-});
+})
 
-module.exports = { schema };
+module.exports = { schema }
