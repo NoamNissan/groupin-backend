c441ee737863fa54d1b368aa49b979b91142892b
non847 <non847@gmail.com>
2020-04-01 15:44:15 +0300

    remove facebook app secret from config file. moved to env file

--- config/facebook_oauth.js
+++ config/facebook_oauth.js
@@ -1,21 +1,21 @@
+dotenv = require('dotenv');
+dotenv.config();
 module.exports = {
     development: {
-        client_id : '896021850846067',
-        client_secret : '68a51cdd4996f09291caa74df18468e0',
-        callback_url : 'http://localhost:4000/users/auth/facebook/callback',
+        client_id: '896021850846067',
+        client_secret: process.env.FACEBOOK_CLIENT_SECRET,
+        callback_url: 'http://localhost:4000/users/auth/facebook/callback'
     },
 
-
     test: {
-        client_id : '896021850846067',
-        client_secret : '68a51cdd4996f09291caa74df18468e0',
-        callback_url : 'http://localhost:4000/users/auth/facebook/callback',
+        client_id: '896021850846067',
+        client_secret: process.env.FACEBOOK_CLIENT_SECRET,
+        callback_url: 'http://localhost:4000/users/auth/facebook/callback'
     },
 
     production: {
-        client_id : process.env.FACEBOOK_CLIENT_ID,
-        client_secret : process.env.FACEBOOK_CLIENT_SECRET,
-        callback_url : process.env.FACEBOOK_CALLBACL_URL,
+        client_id: process.env.FACEBOOK_CLIENT_ID,
+        client_secret: process.env.FACEBOOK_CLIENT_SECRET,
+        callback_url: process.env.FACEBOOK_CALLBACK_URL
     }
 };
-
